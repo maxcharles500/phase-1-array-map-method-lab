adabb954d5e38150a1f5504dd3b31258f3f3d710
maximilianjones <maxcharles500@gmail.com>
2021-11-26 10:26:31 -0800

    commit tree ca7a20c0bf0995ca944ce3a8ab3ed145027edc8a

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    const sentArr = sentence.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+
+    return sentArr.join(' ')
+  })
 }
